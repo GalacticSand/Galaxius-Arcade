8cc29ee9e986c5541da22650320d2aa420ac8e0b
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-07-21 13:08:46 -0400

    Update four-in-a-row.cpp

--- four-in-a-row.cpp
+++ four-in-a-row.cpp
@@ -3,14 +3,45 @@
 #include <string>
 
 // SET UP PRIMARY GLOBAL VARIABLES AND SETS
-bool pterm;
-bool pterm_alt;
 
-std::vector<std::vector<std::string>> {{}, {}};
+bool pterm;          // TERMINATOR FOR MAIN POLLING
+bool pterm_alt;      // TERMINATOR FOR ANY SECONDARY POLLINGS
+int opt;             // STORES USER-CHOSEN OPTION AT SELECTION MENUS
+
+// SET UP SPRITES AND GRAPHICAL ITEMS
+
+std::string m_blank = "[ ]";      // 
+std::string m_rs = "[R]";         // 
+std::string m_ys = "[Y]";         // 
+
+std::vector<int> opt_list;
+std::vector<std::vector<std::string>> ns_grid = {{"|A|", "|B|", "|C|", "|D|", "|E|", "|F|", "|G|"}, 
+                                                 {"---", "---", "---", "---", "---", "---", "---"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"---", "---", "---", "---", "---", "---", "---"}, 
+                                                 {"|_|", "   ", "   ", "|_|", "   ", "   ", "|_|"}};
+std::vector<std::vector<std::string>> ls_grid = {{"|A|", "|B|", "|C|", "|D|", "|E|", "|F|", "|G|", "|H|", "|I|"}, 
+                                                 {"---", "---", "---", "---", "---", "---", "---", "---", "---"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"}, 
+                                                 {"---", "---", "---", "---", "---", "---", "---", "---", "---"}, 
+                                                 {"|_|", "   ", "   ", "   ", "|_|", "   ", "   ", "   ", "|_|"}};
 
 // MAIN SPACE WHERE GAME INITIALIZES AND PRIMARY POLLING TAKES PLACE
+
 int main() {
-    stop = false;
+    pterm = false;
     while (true) {
         if (pterm) { break; }
     }
