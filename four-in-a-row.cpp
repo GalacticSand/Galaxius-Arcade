09502f611df4920e9351e873c88c8cb7eed732e2
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-07-21 12:36:47 -0400

    Create four-in-a-row.cpp

---
+++ four-in-a-row.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// SET UP PRIMARY GLOBAL VARIABLES AND SETS
+bool pterm;
+bool pterm_alt;
+
+std::vector<std::vector<std::string>> {{}, {}};
+
+// MAIN SPACE WHERE GAME INITIALIZES AND PRIMARY POLLING TAKES PLACE
+int main() {
+    stop = false;
+    while (true) {
+        if (pterm) { break; }
+    }
+}
