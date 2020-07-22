39f70fbe1816911a89b5714e705d58882b9bde90
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-07-22 15:58:54 -0400

    Update four-in-a-row.cpp

--- four-in-a-row.cpp
+++ four-in-a-row.cpp
@@ -7,6 +7,7 @@
 
 bool pterm;                    // TERMINATOR FOR MAIN POLLING
 bool pterm_alt;                // TERMINATOR FOR ANY SECONDARY POLLINGS
+bool err;                      // INDICATOR FOR POSSIBLE ERROR RETURN AT SELECTION MENUS
 int opt;                       // STORES USER-CHOSEN OPTION AT SELECTION MENUS
 std::vector<int> opt_list;     // STORES THE LEGAL SELECTION OF OPTIONS AT A GIVEN PROMPT
 
@@ -63,7 +64,10 @@ std::vector<std::vector<std::string>> ls_grid = {{"|A|", "|B|", "|C|", "|D|", "|
                                                  {"---", "---", "---", "---", "---", "---", "---", "---", "---"}, 
                                                  {"|=|", "   ", "|=|", "   ", "|=|", "   ", "|=|", "   ", "|=|"}};
 
-// RENDERING FOR GAME BOARDS
+// RENDERING FOR GAME BOARDS + ADDITIONAL CUSTOM SCREEN COMMANDS
+void s_pause() { std::cin.get(); }                         // SHORTCUT FOR PAUSING THE SCREEN
+void s_clear() { std::cout << "\033[2J\033[1;1H"; }        // SHORTCUT FOR CLEARING THE SCREEN
+
 void ren_grid() {
     for (int i = 0; i < a_grid.size(); i++) {
         std::cout << "::::\t";
@@ -74,8 +78,25 @@ void ren_grid() {
 
 ////// SET UP SCREENS
 
+// SOME ADDITIONAL SELECTION COMMANDS
+bool in_opt(int n) {
+    bool qu = false;
+    for (int i = 0; i < opt_list.size(); i++) { if (opt_list[i] == n) { qu = true; } }
+    return qu;
+}
+
+void ren_sel(int o) {
+    err = false;
+    opt_list = {};
+    for (int i = 0; i < o; i++) { opt_list.push(i + 1); }
+    s_clear();
+}
+
+void ch_err() { if (!in_opt(opt)) { err = true; } }
+
+// SCREEN RENDERS
 void print_titlescr() {
-    system("clear");
+    ren_sel(1);
     std::cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
     std::cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
     std::cout << "::::                                                                     ::::\n";
@@ -94,7 +115,28 @@ void print_titlescr() {
     std::cout << "::::                                                                     ::::\n";
     std::cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
     std::cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"; 
-    std::cin.get();
+    std::cout << "\n\n";
+    std::cout << " -=-=-=-=  Press Enter to Start  =-=-=-=- \n";
+    s_pause();
+}
+
+void print_menuscr() {
+    ren_sel(3)
+    std::cout << "::::\n";
+    std::cout << "::::\n";
+    std::cout << "::::    Connect 4 - Main Menu\n";
+    std::cout << "::::\n";
+    std::cout << "::::\n";
+    std::cout << "::::    1. Begin Game \n";
+    std::cout << "::::    2. Settings \n";
+    std::cout << "::::    3. Quit \n";
+    std::cout << "::::\n";
+    std::cout << "::::\n";
+    std::cout << "::::\n";
+    std::cout << "::::\n";
+    std::cout << "::::    Select Option: ";
+    std::cin >> opt;
+    ch_err();
 }
 
 ////// SET UP FUNCTIONS AND GAMEPLAY
